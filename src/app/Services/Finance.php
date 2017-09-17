481dbc152355fee8e8e0df8d30c7f2c8a6a4670e
Mariano G. Egui <egui.mariano@gmail.com>
2017-09-17 12:10:25 -0300

    fix url, add crawler

--- src/app/Services/Finance.php
+++ src/app/Services/Finance.php
@@ -6,40 +6,45 @@ namespace App\Services;
 use App\Exceptions\FinanceException;
 use Illuminate\Support\Facades\Cache;
 use GuzzleHttp\Client;
+use Symfony\Component\DomCrawler\Crawler;
 
 class Finance
 {
     const DOLLAR_CURRENCY = 'USD';
+    const FINANCE_URI = 'https://finance.google.com/finance/converter';
+    const CACHE_TTL = 5;   //minutes
 
     private $countries = [
-        self::DOLLAR_CURRENCY => [ 'currency' => 'USD', 'country' => 'E.E.U.U.'],
-        'ARG'=> [ 'currency' => 'ARS', 'country' => 'Argentina'],
-        'CHI'=> [ 'currency' => 'CLP', 'country' => 'Chile'],
-        'BOL'=> [ 'currency' => 'BOB', 'country' => 'Bolivia'],
-        'BRA'=> [ 'currency' => 'BRL', 'country' => 'Brasil'],
-        'COL'=> [ 'currency' => 'COP', 'country' => 'Colombia'],
-        'CRI'=> [ 'currency' => 'CRC', 'country' => 'Costa Rica'],
-        'DOM'=> [ 'currency' => 'DOP', 'country' => 'Dominicana'],
-        'ECU'=> [ 'currency' => 'USD', 'country' => 'Ecuador'],
-        'GUA'=> [ 'currency' => 'GTQ', 'country' => 'Guatemala'],
-        'HON'=> [ 'currency' => 'HNL', 'country' => 'Honduras'],
-        'MEX'=> [ 'currency' => 'MXN', 'country' => 'Mexico'],
-        'NIC'=> [ 'currency' => 'NIO', 'country' => 'Nicaragua'],
-        'PAN'=> [ 'currency' => 'PAB', 'country' => 'Panamanian'],
-        'PAR'=> [ 'currency' => 'PYG', 'country' => 'Paraguay'],
-        'PER'=> [ 'currency' => 'PEN', 'country' => 'Peru'],
-        'SAL'=> [ 'currency' => 'SVC', 'country' => 'Salvador'],
-        'URU'=> [ 'currency' => 'UYU', 'country' => 'Uruguay'],
-        'VEN'=> [ 'currency' => 'VEF', 'country' => 'Venezuela'],
+        self::DOLLAR_CURRENCY => ['currency' => 'USD', 'country' => 'E.E.U.U.'],
+        'ARG' => ['currency' => 'ARS', 'country' => 'Argentina'],
+        'CHI' => ['currency' => 'CLP', 'country' => 'Chile'],
+        'BOL' => ['currency' => 'BOB', 'country' => 'Bolivia'],
+        'BRA' => ['currency' => 'BRL', 'country' => 'Brasil'],
+        'COL' => ['currency' => 'COP', 'country' => 'Colombia'],
+        'CRI' => ['currency' => 'CRC', 'country' => 'Costa Rica'],
+        'DOM' => ['currency' => 'DOP', 'country' => 'Dominicana'],
+        'ECU' => ['currency' => 'USD', 'country' => 'Ecuador'],
+        'GUA' => ['currency' => 'GTQ', 'country' => 'Guatemala'],
+        'HON' => ['currency' => 'HNL', 'country' => 'Honduras'],
+        'MEX' => ['currency' => 'MXN', 'country' => 'Mexico'],
+        'NIC' => ['currency' => 'NIO', 'country' => 'Nicaragua'],
+        'PAN' => ['currency' => 'PAB', 'country' => 'Panamanian'],
+        'PAR' => ['currency' => 'PYG', 'country' => 'Paraguay'],
+        'PER' => ['currency' => 'PEN', 'country' => 'Peru'],
+        'SAL' => ['currency' => 'SVC', 'country' => 'Salvador'],
+        'URU' => ['currency' => 'UYU', 'country' => 'Uruguay'],
+        'VEN' => ['currency' => 'VEF', 'country' => 'Venezuela'],
     ];
+    /** @var  Client */
+    private $client;
 
     public function getConverter($countryCode)
     {
-        if (!key_exists($countryCode,$this->countries)){
+        if (!key_exists($countryCode, $this->countries)) {
             throw new FinanceException('Currency is not allowed');
         }
 
-        if (!empty($this->conversion($countryCode))){
+        if (false && !empty($this->conversion($countryCode))) {
             return $this->conversion($countryCode);
         }
 
@@ -59,10 +64,10 @@ class Finance
     public function renderSelect($id, $current)
     {
         $select = '<select name="' . $id . '" id="' . $id . '">' . PHP_EOL;
-        foreach ($this->countries as $code => $data){
+        foreach ($this->countries as $code => $data) {
             $selected = $code == $current ? 'selected' : '';
 
-            $select .= '<option value="' . $code .'" ' . $selected .'>' . $data['country'] . '</option>' . PHP_EOL;
+            $select .= '<option value="' . $code . '" ' . $selected . '>' . $data['country'] . '</option>' . PHP_EOL;
         }
 
         return $select . '</select>';
@@ -82,12 +87,9 @@ class Finance
     {
         $fromCurrency = $this->countries[$fromCountry]['currency'];
         $toCurrency = $this->countries[$toCountry]['currency'];
-        $regex = '/<span class=bld>(.+)\s.+<\/span>/';
-        $minutes = 5;
-        $client = new Client();
-        $res = $client->request(
+        $res = $this->getClient()->request(
             'GET',
-            'https://www.google.com/finance/converter',
+            self::FINANCE_URI,
             [
                 'query' => [
                     'a' => 1,
@@ -98,9 +100,23 @@ class Finance
         );
 
         $content = $res->getBody()->getContents();
-        preg_match($regex,$content,$result);
-        Cache::put(static::generateKey($toCurrency), $result[1], $minutes);
-        return $result[1];
+        $crawler = new Crawler($content);
+        $span = $crawler->filter('span.bld');
+        $value = (float)$span->text();
+
+        Cache::put(static::generateKey($toCurrency), $value, self::CACHE_TTL);
+        return $value;
+    }
+
+    /**
+     * @return Client
+     */
+    private function getClient()
+    {
+        if ($this->client) {
+            return $this->client;
+        }
+        return $this->client = new Client(['cookies' => true]);
     }
 
 }
